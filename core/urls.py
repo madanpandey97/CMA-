1ee044fa920e10886fb991a9184053344e2d77a3
madanpandey97 <madanpandey97@gmail.com>
2020-03-28 10:13:58 +0530

    new changes

---
+++ core/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import (HomeView ,
+                    ItemDetailView,
+                     add_to_cart,
+                     remove_from_cart)
+app_name = 'core'
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('product/<slug>',ItemDetailView.as_view() , name = 'product' ),
+    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
+    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart')
+
+
+]

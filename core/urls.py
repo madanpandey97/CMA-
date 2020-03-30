a6a67a8d6f86656b002f323dac0ae1662b38abb7
madanpandey97 <madanpandey97@gmail.com>
2020-03-31 00:31:27 +0530

    order-summary added

--- core/urls.py
+++ core/urls.py
@@ -2,14 +2,16 @@ from django.urls import path
 from .views import (HomeView ,
                     ItemDetailView,
                      add_to_cart,
-                     remove_from_cart)
+                     remove_from_cart,
+                    OrderSummaryView)
 app_name = 'core'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('product/<slug>',ItemDetailView.as_view() , name = 'product' ),
+    path('product/<slug>',ItemDetailView.as_view(), name = 'product' ),
     path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
-    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart')
+    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
+    path('order-summary/', OrderSummaryView.as_view(), name='order-summary')
 
 
 ]
